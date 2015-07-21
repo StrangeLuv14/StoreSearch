65f8e943ea49804054f7a889fc8ccc37b7377cd4
StrangeLuv <b-seo@hotmail.com>
2015-07-21 22:35:55 +0800

    Added animation to popup view.

--- StoreSearch/DetailViewController.swift
+++ StoreSearch/DetailViewController.swift
@@ -46,6 +46,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         
         view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        view.backgroundColor = UIColor.clearColor()
         popupView.layer.cornerRadius = 10
         
         let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("close"))
@@ -113,6 +114,14 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController: presented, presentingViewController: presenting)
+    }
+    
+    func animationControllerForPresentedController(presented: UIViewController, presentingController presenting: UIViewController, sourceController source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return BounceAnimationController()
+    }
+    
+    func animationControllerForDismissedController(dismissed: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        return SlideOutAnimationController()
     }
 }
 
